d36155fff69c4cfc356397323a73c4612736684e
Pedro Rios <st.pedro@gmail.com>
2020-06-09 15:45:25 -0500

    part00004

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -11,8 +11,20 @@ public class Alice {
                 "was reading, but it had no pictures or conversations in it, 'and what is the use of a " +
                 "book,' thought Alice 'without pictures or conversation?'";
 
+        input = new Scanner(System.in);
         System.out.println("Hello, what word would you like to search?");
         String word = input.nextLine();
         input.close();
+
+        String firstLower= first.toLowerCase();
+        String wordNext = word.toLowerCase();
+
+        boolean contains = firstLower.contains(wordNext);
+
+        if (contains == true) {
+            System.out.println("Congratulations, " + word + " has been found within the text!");
+        } else {
+            System.out.println("My apologies, " + word + " has not been found within the text.");
+        }
     }
 }
